6c677ebe1dfbcc58707ff6665984f4b859ea39db
skynetfrank <frank.uah@gmail.com>
2021-05-24 18:54:49 -0400

    animate.css adding

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,13 @@
 
+// Owl Carousel
+$('.owl-carousel').owlCarousel({
+	animateOut: 'fadeOut',
+	items: 1,
+	loop: true,
+	autoplay: true,
+
+})
+
 // NIVO LIGHTBOX
 /* $('.iso-box-section a').nivoLightbox({
 	effect: 'fadeScale',
@@ -6,6 +15,8 @@
 
 // ISOTOPE FILTER
 jQuery(document).ready(function ($) {
+
+
 
 	if ($('.iso-box-wrapper').length > 0) {
 
@@ -82,4 +93,34 @@ $(document).ready(function () {
 
 		$('html, body').animate({ scrollTop: 0 }, 300);
 	})
+
+
 });
+
+
+
+/* $(".owl-stage").on("beforeChange", function () {
+console.log("carrusel changed!!");
+	$('.caption h1').removeClass('animated fadeInLeft').hide();
+	setTimeout(() => {
+		$('.caption h1').addClass('animated fadeInLeft').show();
+
+	}, 1000);
+
+}) */
+
+var owl = $('.owl-carousel');
+owl.owlCarousel();
+// Listen to owl events:
+
+
+
+
+owl.on('changed.owl.carousel', function (event) {
+
+	$('.caption').removeClass('animate__animated animate__backInDown').hide();
+	setTimeout(() => {
+		$('.caption').addClass('animate__animated animate__backInDown').show();
+	}, 100);
+
+})
